d81c894ee615e0d918ef1351b453bea2cab0af50
ry755 <ryanst755@gmail.com>
2021-04-21 00:36:16 -0700

    Add math_ceil4096

--- kernel/include/math.s
+++ kernel/include/math.s
@@ -28,3 +28,28 @@ math_ceil8:
 .end:
     pop ebx
     ret
+
+; round 32 bit integer up to the nearest multiple of 4096
+; inputs:
+; EAX: number
+; outputs:
+; EAX: rounded number
+math_ceil4096:
+    push ebx
+
+    cmp eax, 0               ; if attempting to round 0, always set to 4096
+    je short .was_zero
+
+    mov ebx, eax
+    and ax, 0b1111100000000000
+
+    cmp eax, ebx
+    je short .end
+
+    add eax, 4096
+    jmp short .end
+.was_zero:
+    mov eax, 4096
+.end:
+    pop ebx
+    ret
