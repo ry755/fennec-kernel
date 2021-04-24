1b47bcf0129d98de01ba947fcb604c87d6555ca7
Lua MacDougall <lua@foxgirl.dev>
2021-04-24 02:49:17 -0700

    Updated build script to compile and link hlmm.c

--- build.sh
+++ build.sh
@@ -8,8 +8,10 @@ echo "assembling stage2"
 nasm bootloader/stage2.s -f bin -o bin/bootloader/stage2.bin
 echo "assembling kernel"
 nasm kernel/kernel.s -i "kernel/include" -g -F dwarf -f elf32 -o bin/kernel/kernel.o
+echo "compiling kernel extensions"
+gcc -m32 -march=i386 -masm=intel -mstackrealign -ffreestanding -fno-pie -nostdlib -Os -c -o bin/kernel/hlmm.o kernel/include/c/hlmm.c
 echo "creating kernel flat binary"
-ld -m elf_i386 -Ttext=0x4000 -e real_start --oformat binary -o bin/kernel/kernel.bin bin/kernel/kernel.o
+ld -m elf_i386 -Ttext=0x4000 -e real_start --oformat binary -o bin/kernel/kernel.bin bin/kernel/kernel.o bin/kernel/hlmm.o
 
 echo "creating kernel symbol file (.text only)"
 nm bin/kernel/kernel.o -p | grep ' T \| t ' | awk '{ print $1" "$3 }' > bin/kernel/kernel.sym
@@ -18,7 +20,7 @@ echo "creating kernel symbol file (all sections, line numbers included)"
 nm bin/kernel/kernel.o -l -p > bin/kernel/kernel_debug.sym
 
 echo "creating boot image"
-./ryfs/ryfs.py create bin/boot.img -l Fennec -s 1474560 -b bin/bootloader/stage1.bin
-./ryfs/ryfs.py add bin/boot.img bin/bootloader/stage2.bin
-./ryfs/ryfs.py add bin/boot.img bin/kernel/kernel.bin
+python3 ryfs/ryfs.py create bin/boot.img -l Fennec -s 1474560 -b bin/bootloader/stage1.bin
+python3 ryfs/ryfs.py add bin/boot.img bin/bootloader/stage2.bin
+python3 ryfs/ryfs.py add bin/boot.img bin/kernel/kernel.bin
 echo "boot image created"
