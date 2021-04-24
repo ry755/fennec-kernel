b7a874e64142bc02af4e239c8da6afbabf347a74
ry755 <ryanst755@gmail.com>
2021-04-24 16:37:18 -0700

    Remove old testing strings

--- kernel/include/strings.s
+++ kernel/include/strings.s
@@ -21,8 +21,6 @@ string_init:            db "kernel init (protected mode)",13,10,0
 string_bss_end:         db "end of .bss: 0x",0
 string_init_pmm:        db "physical memory manager init",13,10,0
 string_pmm_base:        db "pmm_base: 0x",0
-string_paging_alloc_d:  db "allocating physical block for kernel page directory at 0x",0
-string_paging_alloc_t:  db "allocating physical block for kernel page table at 0x",0
 string_init_paging:     db "paging init (yay, it didn't triple fault!)",13,10,0
 string_init_pic:        db "PIC init",13,10,0
 string_init_pit:        db "PIT init (system timer set to 100 Hz)",13,10,0
@@ -34,15 +32,6 @@ string_init_sleeping:   db "sleeping for 2 seconds...",13,10,0
 
 string_info_1:          db "hi :D",0
 string_info_2:          db "        im a smol fennec        ",0
-
-string_vmm_test_1:      db "mapping physical 0x",0
-string_vmm_test_2:      db " to virtual 0x",0
-
-string_pmm_test_1:      db "marking 4KB block at 0x01000000 as used",13,10,0
-string_pmm_test_2:      db "marking 4KB block at 0x01000000 as free",13,10,0
-string_pmm_byte_offset: db "pmm_bitmap byte offset: 0x",0
-string_pmm_bit_number:  db "pmm_bitmap byte offset bit number: 0x",0
-string_pmm_physical:    db "first free physical address: 0x",0
 
 ; warning strings
 string_warn_large_bss:  db "warning: kernel .bss section extends past end of conventional memory",13,10,0
