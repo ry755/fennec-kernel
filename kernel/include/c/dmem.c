754a321c2b0d3c8e912f5f55662c32c3db6f2595
Lua MacDougall <lua@foxgirl.dev>
2021-04-25 10:55:10 -0700

    Updated dmem.c to use new macros in system.h

--- kernel/include/c/dmem.c
+++ kernel/include/c/dmem.c
@@ -1,26 +1,28 @@
 #include "dmem.h"
 #include "hlmm.h"
 
-static hlmm_ctx *ctx = NULL;
+static hlmm_ctx *kheap = NULL;
 
 static inline void *allocate(void *ptr, size_t size) {
-  if (ctx == NULL) {
-    ctx = hlmm_create(0);
-    if (ctx == NULL) return NULL;
+  if (kheap == NULL) {
+    kheap = hlmm_create(0);
+    if (kheap == NULL) return NULL;
   }
-  return hlmm_allocate(ctx, ptr, size);
+  return hlmm_allocate(kheap, ptr, size);
 }
 
 void *malloc(size_t size) {
-  return allocate(NULL, size == 0 ? 1 : size);
+  return allocate(NULL, max(size, (size_t)1));
 }
+
 void *calloc(size_t num, size_t size) {
-  size *= num;
-  return allocate(NULL, size == 0 ? 1 : size);
+  return allocate(NULL, max(size * num, (size_t)1));
 }
+
 void *realloc(void *ptr, size_t size) {
   return allocate(ptr, size);
 }
+
 void free(void *ptr) {
   allocate(ptr, 0);
 }
