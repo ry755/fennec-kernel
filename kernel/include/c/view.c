a9a7ff5a52b3b39ebfe74fc801f2ff3522d96a81
Ry <ryanst755@gmail.com>
2024-04-01 17:53:01 -0700

    kernel: Fix views

--- kernel/include/c/view.c
+++ kernel/include/c/view.c
@@ -10,7 +10,7 @@ void view_render(framebuffer_t *target, view_t *view_first) {
     do {
         view_old = view_current;
 
-        view_render((framebuffer_t *) view_current, view_current->next_child);
+        view_render(view_current->framebuffer, view_current->next_child);
         view_copy(target, view_current);
 
         view_current = view_current->next;
@@ -21,12 +21,10 @@ void view_copy(framebuffer_t *target, view_t *source) {
     int16_t source_y = 0;
     int16_t source_x = 0;
     for (int16_t y = source->y; y < (source->y + source->height); y++) {
-        if (y >= target->height || y < 0)
-            continue;
+        if (y >= target->height || y < 0) continue;
         for (int16_t x = source->x; x < (source->x + source->width); x++) {
-            if (x >= target->width || x < 0)
-                continue;
-            target->memory[y * target->width + x] = source->framebuffer->memory[source_y * source->width + source_x];
+            if (x >= target->width || x < 0) continue;
+            target->memory[y * target->width + x] = source->framebuffer->memory[source_y * source->framebuffer->width + source_x];
             source_x++;
         }
         source_x = 0;
